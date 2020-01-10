25de3f724c874e5806e8918df8817faa4550f3d4
Islomzoda <Islomzoda17@gmail.com>
2020-01-10 13:37:07 +0200

    Кэшбэк при оплате по карты

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+
+
+
+let obj = {
+    obj1: {
+         sum: 5500,
+         category: 'regular',
+     },
+     obj2: {
+         sum: 3920,
+         category: 'increased',
+     },
+     obj3: {
+         sum: 1980,
+         category: 'special',
+     }
+ };
+ const regular = 0.01
+ const increased = 0.05;
+ const special = 0.3;
+ 
+ if(obj.obj1.category === 'regular'){
+  ans = obj.obj1.sum * regular;
+ }
+ 
+ if(obj.obj2.category === 'increased'){
+     ans = obj.obj2.sum * increased;
+    }
+ 
+ if(obj.obj3.category === 'special'){
+     ans = obj.obj3.sum * special;
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+ console.log(obj);
+ console.log(ans + ' рублей');
